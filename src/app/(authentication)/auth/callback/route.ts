5689920d29fc368f1286a33983fa707568d96e49
Yanuar Lukman Arif <yanuarlukmanarif@gmail.com>
2025-01-13 09:19:39 +0700

    Update authentication callback to remove default redirect code, ensuring proper handling of redirect parameters.

--- src/app/(authentication)/auth/callback/route.ts
+++ src/app/(authentication)/auth/callback/route.ts
@@ -4,7 +4,7 @@ import { createClient } from "@/app/utils/supabase/server";
 
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url);
-  const code = searchParams.get("code") ?? "/dashboard";
+  const code = searchParams.get("code");
   // Changed default redirect to /dashboard instead of /
   const next = searchParams.get("next") ?? "/dashboard";
 
