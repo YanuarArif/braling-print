55b3396e9db3466a9d9002fdb6f2e1b48610db34
Yanuar Lukman Arif <yanuarlukmanarif@gmail.com>
2025-01-13 15:16:15 +0700

    Improve Supabase client error handling by adding a check for missing environment variables. Log an error message if NEXT_PUBLIC_SUPABASE_URL or NEXT_PUBLIC_SUPABASE_ANON_KEY are not set, enhancing debugging capabilities.

--- src/app/utils/supabase/client.ts
+++ src/app/utils/supabase/client.ts
@@ -1,6 +1,12 @@
 import { createBrowserClient as _createBrowserClient } from "@supabase/ssr";
 
 export function createBrowserClient() {
+  if (
+    !process.env.NEXT_PUBLIC_SUPABASE_URL ||
+    !process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  ) {
+    console.error("Missing Supabase environment variables");
+  }
   return _createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
