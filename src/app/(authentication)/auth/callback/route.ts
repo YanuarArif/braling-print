68d4a3891fa3304cdf9db8c9352399061dc02da7
Yanuar Lukman Arif <yanuarlukmanarif@gmail.com>
2025-01-13 14:41:08 +0700

    add clog env di callback/route.ts

--- src/app/(authentication)/auth/callback/route.ts
+++ src/app/(authentication)/auth/callback/route.ts
@@ -8,6 +8,8 @@ export async function GET(request: Request) {
   // if "next" is in param, use it as the redirect URL
   const next = searchParams.get("next") ?? "/dashboard";
 
+  console.log(process.env);
+
   if (code) {
     const supabase = await createClient();
     const { error } = await supabase.auth.exchangeCodeForSession(code);
