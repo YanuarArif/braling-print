12a98bf62a73eead3dcde151038759c3db7f46f3
Yanuar Lukman Arif <yanuarlukmanarif@gmail.com>
2025-01-13 14:45:07 +0700

    clog tambah banyak

--- src/app/(authentication)/auth/callback/route.ts
+++ src/app/(authentication)/auth/callback/route.ts
@@ -5,28 +5,38 @@ import { createClient } from "@/app/utils/supabase/server";
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url);
   const code = searchParams.get("code");
-  // if "next" is in param, use it as the redirect URL
   const next = searchParams.get("next") ?? "/dashboard";
 
-  console.log(process.env);
+  console.log("OAuth Code:", code);
+  console.log("Supabase API Key: ", process.env.SUPABASE_API_KEY);
 
   if (code) {
-    const supabase = await createClient();
-    const { error } = await supabase.auth.exchangeCodeForSession(code);
-    if (!error) {
-      const forwardedHost = request.headers.get("x-forwarded-host"); // original origin before load balancer
-      const isLocalEnv = process.env.NODE_ENV === "development";
-      if (isLocalEnv) {
-        // we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
-        return NextResponse.redirect(`${origin}${next}`);
-      } else if (forwardedHost) {
-        return NextResponse.redirect(`https://${forwardedHost}${next}`);
+    try {
+      const supabase = await createClient();
+      console.log("Supabase Client Initialized:", !!supabase);
+
+      const { error } = await supabase.auth.exchangeCodeForSession(code);
+      console.log("Exchange Code Result:", error);
+
+      if (!error) {
+        const forwardedHost = request.headers.get("x-forwarded-host");
+        const isLocalEnv = process.env.NODE_ENV === "development";
+
+        if (isLocalEnv) {
+          return NextResponse.redirect(`${origin}${next}`);
+        } else if (forwardedHost) {
+          return NextResponse.redirect(`https://${forwardedHost}${next}`);
+        } else {
+          return NextResponse.redirect(`${origin}`);
+        }
       } else {
-        return NextResponse.redirect(`${origin}$`);
+        console.error("Exchange Code Error:", error);
       }
+    } catch (err) {
+      console.error("Unexpected Error:", err);
     }
   }
 
-  // return the user to an error page with instructions
+  console.error("Invalid Code or Exchange Failed");
   return NextResponse.redirect(`${origin}/auth/auth-code-error`);
 }
